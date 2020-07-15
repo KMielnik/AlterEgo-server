af4b4bb37e7f4f265835358bd1a6bf33345cb7d7
Kamil Mielnik <kamil.mielnik.km@gmail.com>
2020-07-15 16:53:53 +0200

    Init

---
+++ AlterEgo.Core/Domains/Image.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlterEgo.Core.Domains
+{
+    public class Image : MediaResource
+    {
+    }
+}
---
+++ Tester/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using AlterEgo.Core.Domains;
+
+namespace Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var image = new Image();
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
