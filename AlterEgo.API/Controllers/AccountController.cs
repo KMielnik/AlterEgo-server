625d50cc94158edc8a2f280b4f91fc99adfbabfd
Kamil Mielnik <kamil.mielnik.km@gmail.com>
2020-08-10 23:44:51 +0200

    IAccountService

---
+++ AlterEgo.API/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+using AlterEgo.Core.DTOs.Account;
+using AlterEgo.Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlterEgo.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [HttpPost, Route("authenticate")]
+        public async Task<IActionResult> AuthenticateAsync(AuthenticationRequest request)
+            => Ok(await _accountService.AuthenticateAsync(request));
+
+        [HttpPost, Route("register")]
+        public async Task<IActionResult> RegisterAsync(RegisterRequest request)
+        {
+            await _accountService.RegisterAsync(request);
+            return Ok();
+        }
+    }
+}
